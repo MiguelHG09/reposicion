701c38641abd049583c7c66372de5d107367b3e9
MiguelHG09 <miguelhg09@gmail.com>
2024-08-22 21:52:09 -0600

    v4

--- src/main/java/unah/lenguajes/reposicion/controladores/EquipoController.java
+++ src/main/java/unah/lenguajes/reposicion/controladores/EquipoController.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import unah.lenguajes.reposicion.modelos.Equipo;
-import unah.lenguajes.reposicion.servicios.EquipoServicio;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +14,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import unah.lenguajes.reposicion.modelos.Equipo;
+import unah.lenguajes.reposicion.servicios.EquipoServicio;
 
 @RestController
 @RequestMapping("/api/equipos")
