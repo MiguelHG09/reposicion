5e6e424151e12fea27f5ce9cf246bab42e5cea37
MiguelHG09 <miguelhg09@gmail.com>
2024-08-22 20:09:01 -0600

    v1

---
+++ src/test/java/unah/lenguajes/reposicion/ReposicionApplicationTests.java
@@ -0,0 +1,13 @@
+package unah.lenguajes.reposicion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReposicionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
